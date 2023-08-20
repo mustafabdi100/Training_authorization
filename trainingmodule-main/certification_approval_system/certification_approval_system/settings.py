3324ca69314dcbb739c34f0a162568d3d67f1264
mustafabdi100 <mustafa300abdi@gmail.com>
2023-08-20 23:19:19 +0300

    'latestupdate'

--- trainingmodule-main/certification_approval_system/certification_approval_system/settings.py
+++ trainingmodule-main/certification_approval_system/certification_approval_system/settings.py
@@ -65,8 +65,12 @@ WSGI_APPLICATION = 'certification_approval_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'django-backend.coj1tw77hppy.eu-north-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
